25e2ffa84530ba099a2bb7a2d16edb6cb585026b
Shane Carveo <mixedpuppy@gmail.com>
2014-09-10 14:20:47 -0400

    reorg directory structure

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname+"/static/"));
 
 app.start = function(serverPort, callback) {
   server = http.createServer(this);
